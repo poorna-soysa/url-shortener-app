d47f6800b1168ae154fd70ade958de5ba4061c26
Poorna Soysa <poornapiumal.soysa@gmail.com>
2024-11-23 17:01:12 +0530

    Added Redis Cache

--- url-shortener.AppHost/Program.cs
+++ url-shortener.AppHost/Program.cs
@@ -4,7 +4,12 @@ var shortenUrlDb = builder.AddPostgres("shortenUrl-db")
     .WithDataVolume()
     .WithPgAdmin();
 
+var shortenUrlCache = builder.AddRedis("shortenUrl-cache")
+    .WithDataVolume()
+    .WithRedisCommander();
+
 builder.AddProject<Projects.UrlShortener_Api>("urlshortener-api")
-    .WithReference(shortenUrlDb);
+    .WithReference(shortenUrlDb)
+    .WithReference(shortenUrlCache);
 
 builder.Build().Run();
