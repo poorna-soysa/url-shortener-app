497090b36a3ec7cf61059804e098569089dca41e
Poorna Soysa <poornapiumal.soysa@gmail.com>
2024-11-24 10:56:11 +0530

    Removed unwanted comments

--- UrlShortener.Api/Program.cs
+++ UrlShortener.Api/Program.cs
@@ -2,8 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Services.AddScoped<IShortenUrlService, ShortenUrlService>();
@@ -19,7 +17,6 @@ var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
